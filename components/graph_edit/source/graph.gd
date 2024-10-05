5d9d8cfac7210a7be26253445d04c2bf029df301
Rahov <georgi.rahov@siemens.com>
2024-10-05 11:47:49 +0100

    condition button

--- components/graph_edit/source/graph.gd
+++ components/graph_edit/source/graph.gd
@@ -38,7 +38,6 @@ func on_close(dialogueNode: GraphNode) -> void:
 	dialogueNode.queue_free()
 	return
 
-
 func on_connection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int) -> void:
 	connect_node(from_node, from_port, to_node, to_port)
 	return
---
+++ components/graph_node/source/type_menu.gd
@@ -0,0 +1,15 @@
+extends MenuButton
+
+var _popup: PopupMenu = get_popup()
+var _prevIdx: int = 0
+
+func _ready() -> void:
+    _popup.connect("id_pressed", on_pressed)
+    return
+
+func on_pressed(idx: int) -> void:
+    set_text(_popup.get_item_text(idx))
+    _popup.set_item_checked(idx, true)
+    _popup.set_item_checked(_prevIdx, false)
+    _prevIdx = idx
+    return
